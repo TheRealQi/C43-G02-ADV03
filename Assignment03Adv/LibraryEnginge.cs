89399f70a0e28a35d102d067663fa36f11267a42
TheRealQi <therealqi@hotmail.com>
2025-02-05 21:40:22 +0200

    Initial commit

---
+++ Assignment03Adv/LibraryEnginge.cs
@@ -0,0 +1,13 @@
+﻿namespace Assignment03Adv
+{
+    public class LibraryEngine
+    {
+        public static void ProcessBooks(List<Book> bList, BookFunctionDelegate fPtr)
+        {
+            foreach (Book B in bList)
+            {
+                Console.WriteLine(fPtr(B));
+            }
+        }
+    }
+}
---
+++ Assignment03Adv/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Assignment03Adv
+{
+    public delegate string BookFunctionDelegate(Book B);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> books = new List<Book>
+            {
+                new Book("123456789", "C# Advanced", new string[] { "Eng. Aliaa Tarek" }, new DateTime(2025, 1, 30),
+                    1000),
+                new Book("987654321", "C# Basics", new string[] { "Eng. Khaled Ahmed" }, new DateTime(2025, 1, 30),
+                    500),
+            };
+
+
+            #region a. User Defined Delegate Datatype
+
+            BookFunctionDelegate titleDelegate = new BookFunctionDelegate(BookFunctions.GetTitle);
+            LibraryEngine.ProcessBooks(books, titleDelegate);
+
+            #endregion
+
+            #region b. BCL Delegates
+
+            Func<Book, string> authorDelegate = BookFunctions.GetAuthors;
+            LibraryEngine.ProcessBooks(books, new BookFunctionDelegate(authorDelegate));
+
+            #endregion
+
+            #region c. Anonymous Method (GetISBN)
+
+            BookFunctionDelegate isbnDelegate = delegate(Book b) { return b.ISBN; };
+            LibraryEngine.ProcessBooks(books, isbnDelegate);
+            
+            
+            #endregion
+
+            #region d. Lambda Expression (GetPublicationDate)
+            
+            BookFunctionDelegate GetPublicationDate = B => B.PublicationDate.ToString("yyyy-MM-dd");
+            LibraryEngine.ProcessBooks(books, GetPublicationDate);
+            
+            #endregion
+        }
+    }
+}
